7632f959d5fcf54a936a2753c13eb691341200b3
Seyi <setoryz@gmail.com>
2026-01-19 23:12:14 +0000

    chore: update resources

--- variables.tf
+++ variables.tf
@@ -35,14 +35,16 @@ variable "vm_resources" {
     source        = optional(string)
   }))
   default = {
-    control_plane = { cores = 2, memory = 4096, disk = 24 }
-    worker_large  = { cores = 2, memory = 6144, balloon = 4096, disk = 48, longhorn_disk = 256 }
+    control_plane = { cores = 2, memory = 8192, balloon = 6144, disk = 24 }
+    worker_large  = { cores = 2, memory = 8192, balloon = 6144, disk = 48, longhorn_disk = 256 }
     worker_small  = { cores = 1, memory = 6144, balloon = 4096, disk = 36, longhorn_disk = 256 }
     node_small    = { cores = 1, memory = 4096, disk = 1024 }
 
     vm_micro           = { cores = 1, memory = 1024, balloon = 512, disk = 8 }
-    vm_medium          = { cores = 2, memory = 4096, balloon = 1024, disk = 36 }
-    vm_medium_delegate = { cores = 4, memory = 4096, balloon = 1024, disk = 24 }
+    vm_medium          = { cores = 2, memory = 2048, balloon = 1024, disk = 36 }
+    vm_micro_docker    = { cores = 1, memory = 4096, balloon = 2048, disk = 16 }
+    vm_medium_docker   = { cores = 2, memory = 6144, balloon = 4096, disk = 36 }
+    vm_medium_delegate = { cores = 4, memory = 8192, balloon = 6144, disk = 36 }
 
     storage_hdd_large = { size = 1024, source = "storage-hdd" }
   }
@@ -55,7 +57,7 @@ variable "control_planes" {
   }))
 
   default = [
-    { name = "k8s-ctrl-0", node = "pve-main" },
+    { name = "k8s-ctrl-0", node = "pve-n13" },
   ]
 }
 
@@ -72,8 +74,8 @@ variable "worker_nodes" {
     { name = "k8s-worker-1", node = "pve-main", type = "large", index = 1, longhorn_storage = "thinpool-ssd" },
     { name = "k8s-worker-2", node = "pve-main", type = "large", index = 2, longhorn_storage = "thinpool-ssd" },
 
-    { name = "k8s-worker-3", node = "pve-node-1", type = "large", index = 3, longhorn_storage = "local-lvm" },
-    { name = "k8s-worker-sm-0", node = "pve-node-1", type = "small", index = 0, longhorn_storage = "local-lvm" },
+    { name = "k8s-worker-3", node = "pve-n11", type = "large", index = 3, longhorn_storage = "local-lvm" },
+    { name = "k8s-worker-sm-0", node = "pve-n11", type = "small", index = 0, longhorn_storage = "local-lvm" },
   ]
 }
 
@@ -86,7 +88,6 @@ variable "nfs_nodes" {
   }))
 
   default = [
-    { name = "k8s-nfs-storage", node = "pve-node-1", type = "small", storage = "local-lvm" }
   ]
 }
 
@@ -101,6 +102,7 @@ variable "hl_vm_nodes" {
     vm_id_suffix = optional(number)
     extra_disk   = optional(string)
     extra_disks  = optional(list(string))
+    tags         = optional(string)
   }))
 
   default = [
